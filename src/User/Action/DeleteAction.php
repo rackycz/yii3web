e417d51156fbf541121ca7f671e945d05c7df3c9
Radovan <radin.cerny@gmail.com>
2026-09-01 14:59:11 +0200

    User Crud - Deleting works

--- src/User/Action/DeleteAction.php
+++ src/User/Action/DeleteAction.php
@@ -5,32 +5,44 @@ declare(strict_types=1);
 namespace App\User\Action;
 
 use App\Entity\QueryBuilder\UserQueryBuilderRepository;
+use App\Entity\Repository\UserTokenRepository;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Yiisoft\Yii\View\Renderer\WebViewRenderer;
+use Yiisoft\Router\CurrentRoute;
+use Yiisoft\Router\UrlGeneratorInterface;
 
 final readonly class DeleteAction
 {
     public function __construct(
-        private WebViewRenderer            $viewRenderer,
         private UserQueryBuilderRepository $userRepository,
+        private UserTokenRepository        $userTokenRepository,
+        private CurrentRoute               $currentRoute,
+        private UrlGeneratorInterface      $urlGenerator,
+        private ResponseFactoryInterface   $responseFactory,
     )
     {
     }
 
     public function __invoke(ServerRequestInterface $request): ResponseInterface
     {
-        $id = (int)$request->getAttribute('id');
+        $id = (int)$this->currentRoute->getArgument('id');
 
         $user = $this->userRepository->findOne($id);
+
         if ($user === null) {
             throw new \RuntimeException('User not found');
         }
 
-        // For now, render a confirmation page
-        // In a real app, you'd handle POST requests for actual deletion
-        return $this->viewRenderer->render('@user/delete', [
-            'user' => $user,
-        ]);
+
+        $this->userTokenRepository->delete(['id_user' => $id]);
+        $this->userRepository->delete($id);
+
+        return $this->responseFactory
+            ->createResponse(303)
+            ->withHeader(
+                'Location',
+                $this->urlGenerator->generate('user/index'),
+            );
     }
 }
